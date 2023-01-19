d065175d42ff0fbc024a452e56836ed008b07ba0
IK Manding <github@ianmanding.net>
2023-01-19 16:53:52 +0800

    first major release

--- regression_model/processing/data_manager.py
+++ regression_model/processing/data_manager.py
@@ -6,7 +6,7 @@ import pandas as pd
 from sklearn.pipeline import Pipeline
 
 from regression_model import __version__ as _version
-from regression_model.config.core import DATASET_DIR, TRAINED_MODEL_DIR, config
+from regression_model.config.core import TRAINED_MODEL_DIR, DATASET_DIR, config
 
 
 def load_dataset(*, file_name: str) -> pd.DataFrame:
